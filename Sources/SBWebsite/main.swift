b1ce47c086b6821ca43fbf326221a5b30cc62490
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2016-11-02 12:09:01 +0100

    updated to support Vapor 1.1.8
    - droplet init was updated
    - middleware is appended
    - no middleware is taken from .json-file

--- Sources/SBWebsite/main.swift
+++ Sources/SBWebsite/main.swift
@@ -11,9 +11,19 @@ import SwiftyBeaver
 // see http://docs.swiftybeaver.com/category/8-logging-destinations to learn more
 let console = ConsoleDestination()  // log to Xcode Console in color
 let file = FileDestination()  // log to file
-file.logFileURL = URL(string: "file:///tmp/sbwebsite.log")! // set log file
+file.logFileURL = URL(fileURLWithPath: "/tmp/sbwebsite.log") // set log file
 file.minLevel = .debug // no .verbose logging to file
 let sbProvider = SwiftyBeaverProvider(destinations: [console, file])
+
+
+// MARK: Droplet
+
+// create Droplet & add provider
+let app = Droplet()
+app.addProvider(sbProvider)
+
+// shortcut to avoid writing app.log all the time
+let log = app.log.self
 
 
 // MARK: Middleware
@@ -21,24 +31,18 @@ let sbProvider = SwiftyBeaverProvider(destinations: [console, file])
 // use 404.leaf for notFound status and error.leaf for all other errors
 // errorPageMiddleware must be added to droplet via availableMiddleware to be called
 // after file middleware! That avoids throwing 404 errors for files that are served from Public folder
-var errorPageMiddleware = ErrorPageMiddleware("error", errorViews: [Status.notFound: "404"])
+let errorPageMiddleware = ErrorPageMiddleware("error", errorViews: [Status.notFound: "404"])
+// set cache control for .html files to a short TTL and everything else to a long TTL
+let cacheControlMiddleware = CacheControlMiddleware(1800, longTTL: 2592000)
+// allow cross-origin-resources-requests from docs for webfonts and assets
+let corsMiddleWware = CorsMiddleware("http://docs.swiftybeaver.com", pathPatterns: ["webfonts", ".png", ".jpg", ".css"])
 
-var middleware: [String: Middleware]? = [
-    // set cache control for HTML files to a short TTL and everything else to a long TTL
-    "cache-control": CacheControlMiddleware(1800, longTTL: 2592000),
-    // allow CORS requests from docs for webfonts and assets
-    "cors": CorsMiddleware("http://docs.swiftybeaver.com", pathPatterns: ["webfonts", ".png", ".jpg", ".css"]),
-    // serve error pages
-    "error-page": errorPageMiddleware,
-    // WIP: remove key Server from response header to hide Vapor as server
-    //"incognito": IncognitoMiddleware()
-    
-]
+// add middlewares to droplet. FileMiddleWare has to come at the end to make 404 pages work
+app.middleware.append(cacheControlMiddleware)
+app.middleware.append(corsMiddleWware)
+app.middleware.append(errorPageMiddleware)
+app.middleware.append(FileMiddleware(publicDir: "Public")) // serve static files from Public folder
 
-// MARK: Droplet
-
-let app = Droplet(availableMiddleware: middleware, initializedProviders:[sbProvider])
-let log = app.log.self // to avoid writing app.log all the time
 errorPageMiddleware.droplet = app // required for error view rendering
 
 
